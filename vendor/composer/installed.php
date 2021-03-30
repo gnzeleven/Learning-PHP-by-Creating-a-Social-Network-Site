f614e88613079fb0e76a97749febec04044bb301
gnzeleven <vsav4796@gmail.com>
2021-03-29 21:50:45 -0500

    new heroku config

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8827056e219d3fde3c06e2b2c06954a38f42615e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8827056e219d3fde3c06e2b2c06954a38f42615e',
+    ),
+  ),
+);
