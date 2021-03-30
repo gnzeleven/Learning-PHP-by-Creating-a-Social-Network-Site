d672c7b77343293708eb276392283682b49341b5
gnzeleven <vsav4796@gmail.com>
2021-03-29 21:12:04 -0500

    'update'

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-  $dbhost = 'localhost';
-  $dbname = 'sndb';
-  $dbuser = 'user_db';
-  $dbpassword = 'user_pass';
+//mysql://bd93ebaa8a22c6:7d2ce848@us-cdbr-east-03.cleardb.com/heroku_3f53661380c5413?reconnect=true
+  $dbhost = 'us-cdbr-east-03.cleardb.com';
+  $dbname = 'heroku_3f53661380c5413';
+  $dbuser = 'bd93ebaa8a22c6';
+  $dbpassword = '7d2ce848';
   $appname = 'Old School Cool';
 ?>
